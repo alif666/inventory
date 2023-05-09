cc5a05365f2552b3703a581f9424d42e32b76c17
alif666 <aliflodi@gmail.com>
2023-05-10 03:41:08 +0600

    updated SqlJ

--- service/repositories/db_setup.sql
+++ service/repositories/db_setup.sql
@@ -1,16 +1,18 @@
-CREATE TABLE IF NOT EXISTS device (
-  device_id INT AUTO_INCREMENT PRIMARY KEY,
-  device_sl VARCHAR(255) NOT NULL,
-  device_name VARCHAR(255) NOT NULL,
-  device_category VARCHAR(255) NOT NULL,
-  device_req_date DATE NOT NULL,
-  remark VARCHAR(255),
-  status VARCHAR(255) NOT NULL DEFAULT 'active',
-  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  created_by VARCHAR(255) NOT NULL,
-  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  updated_by VARCHAR(255) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `device` (
+  `device_id` int NOT NULL AUTO_INCREMENT,
+  `device_sl` varchar(255) NOT NULL,
+  `device_name` varchar(255) DEFAULT NULL,
+  `device_category` varchar(255) DEFAULT NULL,
+  `device_req_date` date DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'active',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(255) DEFAULT NULL,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_by` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`device_id`),
+  UNIQUE KEY `device_sl_UNIQUE` (`device_sl`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS person (
   person_id INT AUTO_INCREMENT PRIMARY KEY,
